4aa8d8c4d2d5679255fdd343cf5ca00c471102b0
fabiomedeiros <fmauff1978@gmail.com>
2024-05-02 10:32:06 -0300

    primeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,BrowserAnimationsModule,
+    FormsModule,
+    NgxPaginationModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideDatabase(() => getDatabase()),
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'student-52a48',
+    appId: '1:848671945450:web:db60f13321c51fafc55d93',
+    databaseURL: 'https://student-52a48-default-rtdb.firebaseio.com',
+    storageBucket: 'student-52a48.appspot.com',
+    apiKey: 'AIzaSyCvRo_EkgEt76BEbgOR3XafFgblCkzzKYQ',
+    authDomain: 'student-52a48.firebaseapp.com',
+    messagingSenderId: '848671945450',
+  },};
